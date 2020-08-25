059d9ff20b8135c1c54ce8a862bd147e734cd058
ogafer <tiago.dm.ferreira@hotmail.com>
2020-08-25 18:05:45 +0100

    Creating class Position

---
+++ ChessGame/ChessGame/Board/Position.cs
@@ -0,0 +1,30 @@
+﻿namespace Board
+{
+    class Position
+    {
+        #region Attributes
+
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public Position (int row, int column)
+        {
+            this.Row = row;
+            this.Column = column;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public override string ToString()
+        {
+            return Row + "," + Column;
+        }
+        #endregion
+    }
+}
---
+++ ChessGame/ChessGame/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Board;
+
+namespace ChessGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Position position;
+            position = new Position(3, 4);
+
+            Console.WriteLine("Position: " + position);
+            Console.ReadLine();
+        }
+    }
+}
